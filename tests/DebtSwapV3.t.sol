764e89b18f9a26a9ab32d0380b68a9542befba12
Cheyenne Atapour <cheyenneatapour@gmail.com>
2024-10-24 12:19:39 +0100

    fix: V3 without extra collateral test

--- tests/DebtSwapV3.t.sol
+++ tests/DebtSwapV3.t.sol
@@ -60,16 +60,16 @@ contract DebtSwapV3Test is BaseTest {
   }
 
   function test_revert_debtSwap_without_extra_collateral() public {
-    address aToken = AaveV3EthereumAssets.DAI_A_TOKEN;
-    address debtAsset = AaveV3EthereumAssets.DAI_UNDERLYING;
-    address newDebtAsset = AaveV3EthereumAssets.LUSD_UNDERLYING;
-    address newDebtToken = AaveV3EthereumAssets.LUSD_V_TOKEN;
+    address aToken = AaveV3EthereumAssets.WETH_A_TOKEN;
+    address debtAsset = AaveV3EthereumAssets.WETH_UNDERLYING;
+    address newDebtAsset = AaveV3EthereumAssets.DAI_UNDERLYING;
+    address newDebtToken = AaveV3EthereumAssets.DAI_V_TOKEN;
 
-    uint256 supplyAmount = 120e18;
-    uint256 borrowAmount = 80e18;
+    uint256 supplyAmount = 12e18;
+    uint256 borrowAmount = 8e18;
 
-    // We want to end with LT > utilisation > LTV, so we pump up the utilisation to 75% by withdrawing (80 > 75 > 67).
-    uint256 withdrawAmount = supplyAmount - (borrowAmount * 100) / 75;
+    // We want to end with LT > utilisation > LTV, so we pump up the utilisation to 81% by withdrawing (83 > 81 > 80.5).
+    uint256 withdrawAmount = supplyAmount - (borrowAmount * 100) / 81;
 
     // Deal some debtAsset to cover the premium and any 1 wei rounding errors on withdrawal.
     deal(debtAsset, address(debtSwapAdapter), 1e18);
@@ -123,7 +123,7 @@ contract DebtSwapV3Test is BaseTest {
 
   /**
    * 1. supply 200000 DAI
-   * 2. borrow 100 DAI
+   * 2. borrow 1000 DAI
    * 3. swap whole DAI debt to LUSD debt
    */
   function test_debtSwap_swapHalf() public {
